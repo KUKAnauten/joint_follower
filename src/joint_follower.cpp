28ae82bd5c65996bd2a6eec9a1c197b984e3e601
medlab <tututu@tut.tut-darmstut.tut>
2020-11-25 11:59:27 +0100

    Some clean clean up

--- src/joint_follower.cpp
+++ src/joint_follower.cpp
@@ -57,6 +57,7 @@ public:
 				first_time_(true),
         coupled_(true) {
 
+    // Convert degree into radian if necessary
 		if(rad_input_) {
 			angle_conversion_ = 1.0;	
 		}
@@ -64,20 +65,15 @@ public:
 			angle_conversion_ = 3.1416/180.0;
 		}
 
-		// use when base pose is given
-//		base_pose_.position.x = 0.0; //changed base pose -> base in shoulder
-//    base_pose_.position.y = 0.5;
-//    base_pose_.position.z = 0.6;
-//    base_pose_.orientation.x = -sqrt(2.0)/2.0;
-//    base_pose_.orientation.y = 0.0;
-//    base_pose_.orientation.z = 0.0;
-//    base_pose_.orientation.w = sqrt(2.0)/2.0;
 
-		// use when initial joint positions are given
+		// Initial joint positions
+    // Direction of counting and offset -> Joint angles from MCS can be used directly
+
 		iiwa_initial_joint_positions_.joint_names.resize(7);
 		iiwa_initial_joint_positions_.joint_names = RobotInterface::getJointNames();
 		iiwa_initial_joint_positions_.points.resize(1);
 		iiwa_initial_joint_positions_.points[0].positions.resize(7);
+
     // Anthropomorphic. Mirrored
 		// iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * -1.0 * -30.97;
 		// iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (-1.0 * 18.34 + 90.0);
@@ -87,7 +83,7 @@ public:
 		// iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * (-1.0 * -4.63 + 90.0); 
 		// iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
 
-    // Initial positions. Anthropomorphic. Workspace to the side. Not mirrored
+    // Anthropomorphic. Workspace to the side. Not mirrored
     iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * 18.34;
     iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (-30.97 + 90.0);
     iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * (-1.0 * -21.67 - 90.0);
@@ -96,72 +92,12 @@ public:
     iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * (0.0 + 90.0); 
     iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
 
-    // Impedance video
-    // iiwa_initial_joint_positions_.points[0].positions[0] = 0.0955010280013;
-    // iiwa_initial_joint_positions_.points[0].positions[1] = 1.05284523964;
-    // iiwa_initial_joint_positions_.points[0].positions[2] = -0.927038908005;
-    // iiwa_initial_joint_positions_.points[0].positions[3] = 1.33675003052;
-    // iiwa_initial_joint_positions_.points[0].positions[4] = 0.237600877881; 
-    // iiwa_initial_joint_positions_.points[0].positions[5] = 1.35565376282; 
-    // iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-    // iiwa_initial_joint_positions_.points[0].positions[0] = 0.158099394662;
-    // iiwa_initial_joint_positions_.points[0].positions[1] = 1.04273171673;
-    // iiwa_initial_joint_positions_.points[0].positions[2] = -0.930604292196;
-    // iiwa_initial_joint_positions_.points[0].positions[3] = 1.32590148519;
-    // iiwa_initial_joint_positions_.points[0].positions[4] = 0.254567650553; 
-    // iiwa_initial_joint_positions_.points[0].positions[5] = 1.36908613857; 
-    // iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-    // // Initial positions for psychological experiment
-    // iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * 90.0;
-    // iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * 0.0;
-    // iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * 0.0;
-    // iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * -45.0;
-    // iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * 90.0; 
-    // iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * - 90.0; 
-    // iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-		// initial_positions for pick and place, iiwa mounted on tabletop
-//		iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * -8.2299;
-//		iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (-1.0 * -3.0850 + 90.0);
-//		iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * -1.0 * -13.7768;
-//		iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * -1.0 * -52.9783;
-//		iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * (-1.0 * 26.3020 - 90.0); 
-//		iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * 32.7907; 
-//		iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-		// pick and place test
-//		iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * 0.0;
-//		iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (-1.0 * 0.0 + 45.0);
-//		iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * -1.0 * -0.0;
-//		iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * (-1.0 * 0.0 - 90.0);
-//		iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * (-1.0 * 0.0 - 0.0); 
-//		iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * (0.0 - 45.0); 
-//		iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-//		iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * 0.0;
-//		iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * (0.0 - 90.0);
-//		iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * -1.0 * 0.0;
-//		iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * 0.0;
-//		iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * (-1.0 * 0.0 + 90.0); 
-//		iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * 0.0; 
-//		iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 0.0;
-
-    // // Initial positions for parabola movement
-    // iiwa_initial_joint_positions_.points[0].positions[0] = 3.1416/180.0 * 73.36;
-    // iiwa_initial_joint_positions_.points[0].positions[1] = 3.1416/180.0 * 64.80;
-    // iiwa_initial_joint_positions_.points[0].positions[2] = 3.1416/180.0 * -47.49;
-    // iiwa_initial_joint_positions_.points[0].positions[3] = 3.1416/180.0 * -42.24;
-    // iiwa_initial_joint_positions_.points[0].positions[4] = 3.1416/180.0 * -54.34; 
-    // iiwa_initial_joint_positions_.points[0].positions[5] = 3.1416/180.0 * 43.34; 
-    // iiwa_initial_joint_positions_.points[0].positions[6] = 3.1416/180.0 * 98.43;
-
 		mcs_initial_joint_positions_.joint_names.resize(7);
 		mcs_initial_joint_positions_.joint_names = RobotInterface::getJointNames();
 		mcs_initial_joint_positions_.points.resize(1);
 		mcs_initial_joint_positions_.points[0].positions.resize(7);
 
+    // Joint limits
 		upper_joint_limits_.resize(7);
 		lower_joint_limits_.resize(7);
 		upper_joint_limits_[0] = 3.1416/180.0 * 170.0;
@@ -196,10 +132,6 @@ public:
     joint_subscriber_ = node_handle_->subscribe(topic, 1, &JointFollower::jointCallbackAbsolute, this);
   }
 
-  // void registerStateSubscriber(const std::string& topic) {
-  //   state_subscriber_ = node_handle_->subscribe(topic, 1, &JointFollower::stateCallback, this);
-  // }
-
   void setBasePose(const geometry_msgs::Pose& pose) {
     base_pose_ = pose;
   }
@@ -213,23 +145,10 @@ public:
 		
 		base_pose_joint_positions_.joint_names.resize(7);
 		base_pose_joint_positions_.joint_names = names;
-//		base_pose_joint_positions_.joint_names[0] = "iiwa_joint_1";
-//		ROS_INFO_NAMED("joint_follower", "nach names1");
-//		base_pose_joint_positions_.joint_names[2] = "iiwa_joint_3";
-//		base_pose_joint_positions_.joint_names[3] = "iiwa_joint_4";
-//		base_pose_joint_positions_.joint_names[4] = "iiwa_joint_5";
-//		base_pose_joint_positions_.joint_names[5] = "iiwa_joint_6";
-//		base_pose_joint_positions_.joint_names[6] = "iiwa_joint_7";
+
 		base_pose_joint_positions_.points.resize(1);
 		base_pose_joint_positions_.points[0].positions.resize(7);
 		base_pose_joint_positions_.points[0].positions = initial_angles;
-//		base_pose_joint_positions_.points[0].positions[0] = initial_angles[0];
-//		base_pose_joint_positions_.points[0].positions[1] = initial_angles[1];
-//		base_pose_joint_positions_.points[0].positions[2] = initial_angles[2];
-//		base_pose_joint_positions_.points[0].positions[3] = initial_angles[3];
-//		base_pose_joint_positions_.points[0].positions[4] = initial_angles[4];
-//		base_pose_joint_positions_.points[0].positions[5] = initial_angles[5];
-//		base_pose_joint_positions_.points[0].positions[6] = initial_angles[6];
 
 		base_pose_joint_positions_.points[0].time_from_start = ros::Duration(0.01); // later variable
 	}
@@ -243,7 +162,7 @@ public:
 	}
 
   void createUncoupleService() {
-    // service to couple/uncouple the joint follower  
+    // Service to couple/uncouple the joint follower  
     uncouple_service_ = node_handle_->advertiseService("coupler", &JointFollower::uncouple, this);
     ROS_INFO("Uncouple service created.");
   }
@@ -271,7 +190,6 @@ private:
   ros::ServiceServer uncouple_service_;
   bool coupled_;
   std::vector<double> current_joint_positions_;
-  // ros::Subscriber state_subscriber_;
   ros::ServiceClient joint_positions_client_;
   joint_follower::GetJointPositions get_joint_positions_service_;
 
@@ -298,8 +216,10 @@ private:
         ROS_INFO("Set initial joint positions.");
 			}
 
-      //geometry_msgs::Pose target_pose = base_pose_;
+ 
 			trajectory_msgs::JointTrajectory trajectory_point = base_pose_joint_positions_;
+
+      // Mapping MCS to iiwa 
 
       // In front. Mirrored.
 			// trajectory_point.points[0].positions[0] -= (a1 - mcs_initial_joint_positions_.points[0].positions[0])*angle_conversion_;
@@ -310,7 +230,7 @@ private:
 			// trajectory_point.points[0].positions[5] -= (a7 - mcs_initial_joint_positions_.points[0].positions[6])*angle_conversion_;
 			// trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
 
-      // To the side. Not mirrored.
+      // To the side. Not mirrored. MCS v2?
       trajectory_point.points[0].positions[0] += (a2 - mcs_initial_joint_positions_.points[0].positions[1])*angle_conversion_;
       trajectory_point.points[0].positions[1] += (a1 - mcs_initial_joint_positions_.points[0].positions[0])*angle_conversion_;
       trajectory_point.points[0].positions[2] -= (a3 - mcs_initial_joint_positions_.points[0].positions[2])*angle_conversion_;
@@ -319,14 +239,6 @@ private:
       trajectory_point.points[0].positions[5] -= (a7 - mcs_initial_joint_positions_.points[0].positions[6])*angle_conversion_;
       trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
 
-      // // Psychological experiment. Just q2 and q4
-      // trajectory_point.points[0].positions[0] += 0.0*angle_conversion_;
-      // trajectory_point.points[0].positions[1] += (a2 - mcs_initial_joint_positions_.points[0].positions[1])*angle_conversion_;
-      // trajectory_point.points[0].positions[2] -= 0.0*angle_conversion_;
-      // trajectory_point.points[0].positions[3] += (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
-      // trajectory_point.points[0].positions[4] -= 0.0*angle_conversion_;
-      // trajectory_point.points[0].positions[5] += 0.0*angle_conversion_;
-      // trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
 
       // No adaption for MCS. Just one by one.
       // trajectory_point.points[0].positions[0] += (a1 - mcs_initial_joint_positions_.points[0].positions[0])*angle_conversion_;
@@ -339,22 +251,14 @@ private:
 
 
 
-//			trajectory_point.points[0].positions[0] += (a1 - mcs_initial_joint_positions_.points[0].positions[0])*angle_conversion_;
-//			trajectory_point.points[0].positions[1] += -1.0 * (a2 - mcs_initial_joint_positions_.points[0].positions[1])*angle_conversion_;
-//			trajectory_point.points[0].positions[2] -= (a3 - mcs_initial_joint_positions_.points[0].positions[2])*angle_conversion_;
-//			trajectory_point.points[0].positions[3] += -1.0 * (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
-//			trajectory_point.points[0].positions[4] -= (a5 - mcs_initial_joint_positions_.points[0].positions[4])*angle_conversion_;
-//			trajectory_point.points[0].positions[5] += (a7 - mcs_initial_joint_positions_.points[0].positions[6])*angle_conversion_;
-//			trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
+			// trajectory_point.points[0].positions[0] += (a1 - mcs_initial_joint_positions_.points[0].positions[0])*angle_conversion_;
+			// trajectory_point.points[0].positions[1] += -1.0 * (a2 - mcs_initial_joint_positions_.points[0].positions[1])*angle_conversion_;
+			// trajectory_point.points[0].positions[2] -= (a3 - mcs_initial_joint_positions_.points[0].positions[2])*angle_conversion_;
+			// trajectory_point.points[0].positions[3] += -1.0 * (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
+			// trajectory_point.points[0].positions[4] -= (a5 - mcs_initial_joint_positions_.points[0].positions[4])*angle_conversion_;
+			// trajectory_point.points[0].positions[5] += (a7 - mcs_initial_joint_positions_.points[0].positions[6])*angle_conversion_;
+			// trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
 
-			// pick and place test
-//			trajectory_point.points[0].positions[0] += (a3 - mcs_initial_joint_positions_.points[0].positions[2])*angle_conversion_;
-//			trajectory_point.points[0].positions[1] += 0.5 * (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
-////			trajectory_point.points[0].positions[2] -= (a3 - mcs_initial_joint_positions_.points[0].positions[2])*angle_conversion_;
-//			trajectory_point.points[0].positions[3] += (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
-////			trajectory_point.points[0].positions[4] -= (a5 - mcs_initial_joint_positions_.points[0].positions[4])*angle_conversion_;
-//			trajectory_point.points[0].positions[5] += 0.5 * (a4 - mcs_initial_joint_positions_.points[0].positions[3])*angle_conversion_;
-////			trajectory_point.points[0].positions[6] += 0.0*angle_conversion_;
 
 			trajectory_point = jointLimitation(trajectory_point);
 
@@ -409,11 +313,6 @@ private:
     }
     return true;
   }
-
-//   void stateCallback(const sensor_msgs::JointState::ConstPtr& msg) {
-//     current_joint_positions_ = msg->position;
-// //    ROS_INFO("%.4f %.4f %.4f %.4f %.4f %.4f %.4f", current_joint_positions_[0], current_joint_positions_[1], current_joint_positions_[2], current_joint_positions_[3], current_joint_positions_[4], current_joint_positions_[5], current_joint_positions_[6]);  
-//   }
 	
 };
 } // namespace joint_follower
@@ -433,37 +332,31 @@ int main(int argc, char **argv)
 	
 	double scale_factor; // MOD
 	node_handle.param("/iiwa/joint_follower/scale_factor", scale_factor, 1.0);
-	
-	// input in rad or deg
+
+  // Set parameter from launch	
+	// Input in rad or deg
 	bool rad_input;
 	node_handle.param("/iiwa/joint_follower/rad_input", rad_input, true);
-
-	// input from udp or csv
+	// Input from udp or csv
 	bool udp_input;
 	node_handle.param("/iiwa/joint_follower/udp", udp_input, false);
 
 
   joint_follower::JointFollower joint_follower(&node_handle, "manipulator", "world", scale_factor, 2, rad_input);
 
-  // joint_follower.registerStateSubscriber(std::string("/iiwa/joint_states")); 
+  // Creates the service which (un)couples the iiwa after a request from the client (udp_receiver -> pick_and_place_receive.py)
 	joint_follower.createUncoupleService();
 
   // Create service client to request the joint positions only when needed
   joint_follower.createJointPositionClientAndRequest();
 
-//	// use when base pose is given
-//  joint_follower.moveToBasePose();
-//	joint_names = joint_follower.getJointNames();
-//	initial_joint_positions = joint_follower.getJointPositions();
-//	joint_follower.setBasePoseJointPositions(joint_names, initial_joint_positions);
-
-	// use when initial joint positions are given
-	joint_follower.moveToInitialJointPositions();
-	//joint_follower.setBasePoseToCurrent();		
+  // Move to initial position set it as reference
+	joint_follower.moveToInitialJointPositions();	
 	joint_names = joint_follower.getJointNames();
 	initial_joint_positions = joint_follower.getJointPositions();
 	joint_follower.setBasePoseJointPositions(joint_names, initial_joint_positions);
 
+  // The movement starts afterwards
   joint_follower.waitForApproval();
 	if(udp_input) {
 		joint_follower.registerSubscriberRelative(std::string("/iiwa/jointAnglesFromUDP/JointPosition"));
@@ -473,21 +366,9 @@ int main(int argc, char **argv)
   	joint_follower.registerSubscriberRelative(std::string("/jointAnglesFromFile/JointPosition"));
 		ROS_INFO_NAMED("joint_follower", "Subscribed to set of joint angles from file!");
 	} 
-//  joint_follower.registerSubscriberRelative(std::string("/jointAnglesFromUDP/JointPosition"));
-  //joint_follower.registerSubscriberAbsolute(std::string("/jointFromFile/PoseStampedAbsolute"));
-
-//  if(joint_follower.setJointImpedanceMode(0.7, 0.3)) {
-//		ROS_INFO_NAMED("joint_follower", "Success");
-//	}
-//	else {
-//		ROS_INFO_NAMED("joint_follower", "Failure");
-//	}
 
   ros::Rate rate(100);
   while(ros::ok()) {
-//		current_pose = joint_follower.getPose(std::string("iiwa_link_ee"));
-//		//ROS_INFO("%s", std::to_string(current_pose.pose.position.x).c_str());
-//		pose_pub.publish(current_pose);
     rate.sleep();
   }
   ros::shutdown();
